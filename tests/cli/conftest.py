774acbd33037aa4483531916a5138928f057c329
Rubin Singh <singhrubin08@gmail.com>
2025-06-14 18:03:50 +0530

    batch_yeet refactor

---
+++ tests/cli/conftest.py
@@ -0,0 +1,37 @@
+import pytest
+
+from ase import Atoms
+from ase.io import write
+import numpy as np
+from pathlib import Path
+
+
+read_dir = "out"
+save_dir = "dumped_files"
+
+frames_size = 5
+
+@pytest.fixture(scope="session")
+def read_io_save_path(tmp_path_factory):
+    dump_path = tmp_path_factory.mktemp(read_dir, numbered=False)
+    return dump_path
+
+@pytest.fixture(scope="session")
+def frames_save_setup_path(tmp_path_factory):
+    frames_save_setup_path = tmp_path_factory.mktemp(save_dir, numbered=False)
+    return frames_save_setup_path
+
+@pytest.fixture(scope="session")
+def atoms_records():
+    symbols = ['C', 'H', 'O']
+    atoms_list = []
+    for i in range(frames_size):
+        pos = np.random.rand(len(symbols),3)
+        atoms_list.append(Atoms(symbols=symbols, positions=pos, cell=[10,10,10], pbc=True))
+    return atoms_list
+
+@pytest.fixture(autouse=True)
+def save_frames(atoms_records, frames_save_setup_path):
+    for i, at in enumerate(atoms_records):
+        write(str(frames_save_setup_path/Path(f'config{i}.xyz')), at)
+    print(f"All files written in {str(frames_save_setup_path)}")
--- tests/cli/test_batchyeet.py
+++ tests/cli/test_batchyeet.py
@@ -1,52 +1,18 @@
-import pytest
-
-from ase import Atoms
-from ase.io import write, read
-import numpy as np
 from pathlib import Path
 
 from cli.batch_yeet import validate_arguments, collect_frames, export_config
 
-read_dir = "out"
-save_dir = "dumped_files"
-
-frames_size = 5
-
-@pytest.fixture(scope="session")
-def read_io_save_path(tmp_path_factory):
-    dump_path = tmp_path_factory.mktemp(read_dir, numbered=False)
-    return dump_path
-
-@pytest.fixture(scope="session")
-def frames_save_setup_path(tmp_path_factory):
-    frames_save_setup_path = tmp_path_factory.mktemp(save_dir, numbered=False)
-    return frames_save_setup_path
-
-@pytest.fixture(scope="session")
-def atoms_records():
-    symbols = ['C', 'H', 'O']
-    atoms_list = []
-    for i in range(frames_size):
-        pos = np.random.rand(len(symbols),3)
-        atoms_list.append(Atoms(symbols=symbols, positions=pos, cell=[10,10,10], pbc=True))
-    return atoms_list
-
-@pytest.fixture(autouse=True)
-def save_frames(atoms_records, frames_save_setup_path):
-    for i, at in enumerate(atoms_records):
-        write(str(frames_save_setup_path/Path(f'config{i}.xyz')), at)
-    print(f"All files written in {str(frames_save_setup_path)}")
 
 
 def test_validate_args(frames_save_setup_path):
     validate_arguments(str(frames_save_setup_path), "_")
 
-def test_collect_frames(frames_save_setup_path):
+def test_collect_frames(frames_save_setup_path, atoms_records):
     print(f"Reading from: {frames_save_setup_path}")
     db = collect_frames(frames_save_setup_path)
-    assert len(db) == frames_size
+    assert len(db) == len(atoms_records)
 
-def test_export_frames(mocker, frames_save_setup_path, atoms_records, read_io_save_path):
+def test_export_frames(mocker, atoms_records, read_io_save_path):
     svpath =  read_io_save_path / Path("benzene/10/benzene_10__raw_dataset.xyz")
     mock_full_path = mocker.patch("cli.batch_yeet.RawDataset.full_path")
     mock_full_path.return_value = svpath
