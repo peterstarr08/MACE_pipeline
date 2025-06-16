19eee939ac554627109aabdd15387d8d0c5c79a0
Rubin Singh <singhrubin08@gmail.com>
2025-06-16 13:00:33 +0530

    Fix UniformSelector

--- src/sampler/uniform_selector.py
+++ src/sampler/uniform_selector.py
@@ -1,14 +1,30 @@
 from .base import Selector
 import numpy as np
+from config import us_0_selected, us_off_selected
 
 class UniformSelector(Selector):
     def __init__(self, offset: int = 0):
         super().__init__()
         self.offset = offset
 
+    @staticmethod
+    def clear_initial_selection(configs):
+        for at in configs:
+            for key in (us_0_selected, us_off_selected):
+                at.info.pop(key, None)
+
     def select(self, configs, nframe):
         if len(configs)<nframe:
             raise ValueError(f'Not enough {nframe} frame(s) in Atoms[{len(configs)}] ')
         
-        indices = np.linspace(self.offset, len(configs), nframe, endpoint=False, dtype=int)
-        return [configs[i].copy() for i in indices]
+        indices = np.linspace(0, len(configs), nframe, endpoint=False, dtype=int)
+        offset_indices = np.linspace(self.offset, len(configs), nframe, endpoint=False, dtype=int)
+
+        UniformSelector.clear_initial_selection(configs=configs)
+
+        for i, at in enumerate(configs):
+            if i in indices:
+                at.info[us_0_selected] = True
+            if i in offset_indices:
+                at.info[us_off_selected] = True
+        
--- tests/samplers/test_uniform_selec.py
+++ tests/samplers/test_uniform_selec.py
@@ -1,13 +1,49 @@
 import numpy as np
+import pytest
+from config import us_0_selected, us_off_selected
 from sampler.uniform_selector import UniformSelector
 
 frames_count = 20
-select_frame = 10
 
-def test_select(atoms_records):
-    us = UniformSelector()
+
+
+def test_clear_initial_selection(atoms_records):
     atoms = atoms_records(frames_count)
-    indx = np.linspace(0, len(atoms), select_frame, endpoint=False, dtype=int)
-    db = us.select(atoms, select_frame)
-    for refAt, selAt in zip([atoms[i].copy() for i in indx], db):
-        assert refAt==selAt
+    rnd = np.random.randint(0, frames_count, 5)
+
+    for i in rnd:
+        if i%2==0:
+            atoms[i].info[us_0_selected] = True
+        else:
+            atoms[i].info[us_off_selected] = True
+    
+    UniformSelector.clear_initial_selection(atoms)
+
+    for at in atoms:
+        assert not (us_0_selected in at.info or us_off_selected in at.info)
+
+
+@pytest.mark.parametrize(
+        "select_count, offset",
+        [
+            (15, 5),
+            (12, 0),
+            pytest.param(frames_count+1, 2,marks=pytest.mark.xfail)
+        ]
+)
+def test_select(atoms_records, select_count, offset):
+    atoms = atoms_records(frames_count)
+
+    us = UniformSelector(offset=offset)
+    indx = np.linspace(0, len(atoms), select_count, endpoint=False, dtype=int)
+    offset_indx = np.linspace(offset, len(atoms), select_count, endpoint=False, dtype=int)
+
+    us.select(atoms, select_count)
+
+    for i in range(len(atoms)):
+        if i in indx:
+            assert atoms[i].info[us_0_selected] == True
+        if i in offset_indx:
+            assert atoms[i].info[us_off_selected] == True
+
+
