ba31c3cf9196c0df6685ef6d7b8cd8197a5b6dd5
Rubin Singh <singhrubin08@gmail.com>
2025-06-19 11:37:52 +0530

    Added logs for splitter

--- mace_aze/calculators/xtb.py
+++ mace_aze/calculators/xtb.py
@@ -1,5 +1,7 @@
 from .base import Calculator
 from xtb.ase.calculator import XTB
+from mace_aze.log.conf import get_logger
+log = get_logger(__name__)
 
 DEFAULT_XTB_METHOD = "GFN2-xTB"
 
@@ -10,18 +12,23 @@ xtb_forces_key = "forces_xtb"
 class XTBCalculator(Calculator):
     def __init__(self, method:str = DEFAULT_XTB_METHOD):
         super().__init__()
+        log.info("Instantiating XTB calculator")
+        log.debug("XTB calculator using: method: %s", method)
         self.xtb = XTB(method)
     
     @staticmethod
     def remove_calc(configs):
+        log.info("Almost done. Removing calculators")
         for at in configs:
             at.calc = None
 
     def calculate(self, configs):
+        log.info("Starting XTB calculations. This may take a while")
         for at in configs:
             at.pbc = False
             at.calc = self.xtb
             at.info[xtb_energy_key] = at.get_potential_energy()
             at.arrays[xtb_forces_key] = at.get_forces()
         XTBCalculator.remove_calc(configs)
+        log.info("XTB calculatiosn completed")
         
--- mace_aze/cli/splitter.py
+++ mace_aze/cli/splitter.py
@@ -7,7 +7,9 @@ from mace_aze.sampler import UniformSelector
 from mace_aze.calculators import XTBCalculator
 from mace_aze.utils.records import RawDataset, Dataset
 from mace_aze.config import us_0_selected, us_off_selected
+from mace_aze.log.conf import get_logger
 
+log = get_logger(__name__)
 
 #=================Args=======================
 XTB_CALCULATOR_KEY = 'xtb'
@@ -38,9 +40,13 @@ def arg_parser():
     parser.add_argument('--count', type=int, required=True, help="Number of configs to extract")
     parser.add_argument('--offset', type=int, default=5, help="Offset value for Uniform Selector")
 
+    #Out label
+    parser.add_argument('--out-label', type=str, required=True, help="Output label")
+
     return parser.parse_args()
 
 def validate_arguments(label: str, atoms: list[str], us: bool, offset: int, count: int):
+    log.info("Validating input")
     RawDataset.extract_path(label=label).exists() # Checks if the raw_dataset exists or not using the provided label
     if len(atoms) == 0: # Isolated Atoms cannot be 0
         raise ValueError("No atoms provided. Atoms are needed to calculate E0s")
@@ -60,6 +66,8 @@ def sample_config(db, count: int, us: bool, offset: int):
 
     *Right now only UniformSelector is used
     '''
+    log.info("Sampling configuration")
+    log.debug("Count: %d    UniformSelector: %s     offset:%d", count, str(us), offset)
     key = {
         AUTO_SPLIT: True,
         TRAIN_KEY: DEFAULT_VALUE_SPLIT,
@@ -71,22 +79,27 @@ def sample_config(db, count: int, us: bool, offset: int):
         key[TRAIN_KEY] = us_0_selected
         key[TEST_KEY] = us_off_selected
     # Add more if cases for sampler and change the key to that sampler's sampling
+    log.debug("Auto_split: %s   Train_key: %s   Test_key: %s", key[AUTO_SPLIT], key[TRAIN_KEY], key[TEST_KEY])
     return key
 
 def calculate(db, key: dict, calculator: str, atoms: list[str]):
     selec_conf = []
+    log.info("Extracting samples")
     if key[AUTO_SPLIT] is False: 
         selec_conf = [at for at in db if (key[TRAIN_KEY] in at.info or key[TEST_KEY] in at.info)]
     else:
         selec_conf = db
-    
+    log.info("Extraction completed")
+    log.info("Adding isolated atoms: %s", ' '.join(atoms))
     isolatedAtom = [Atoms(symbol) for symbol in atoms]
     for at in isolatedAtom:
         at.info['config_type'] = 'IsolatedAtom'
         at.info[key[TRAIN_KEY]] = True # To include this in train set
     selec_conf = isolatedAtom + selec_conf
 
+    log.info("Beginning calculations")
     if calculator==XTB_CALCULATOR_KEY:
+        log.info("Using XTB calculator")
         XTBCalculator().calculate(selec_conf)
     
     return selec_conf
@@ -94,13 +107,14 @@ def calculate(db, key: dict, calculator: str, atoms: list[str]):
 def write_configs(*db_lists: list[tuple[Path, list]]):
     for path, db in db_lists:
         path.parent.mkdir()
+        log.info("Writing to %s", str(path))
         write(path, db)
 
 
 def save_config(label: str, db,  calc: str, key: dict):
     train_split = []
     test_split = []
-
+    log.info("Splitting train and test set")
     if key[AUTO_SPLIT]:
         train_split = db[:int(len(db)*AUTO_SPLIT_RATIO)]
         test_split = db[int(len(db)*AUTO_SPLIT_RATIO):]
@@ -110,7 +124,6 @@ def save_config(label: str, db,  calc: str, key: dict):
                 train_split.append(at)
             if key[TEST_KEY] in at.info:
                 test_split.append(at)
-
     train_path = Dataset.extract_path(label=label+"_train", operation=calc)
     test_path = Dataset.extract_path(label=label+"_test", operation=calc)
     write_configs((train_path, train_split), (test_path, test_split))
@@ -118,7 +131,7 @@ def save_config(label: str, db,  calc: str, key: dict):
 def main():
     args = arg_parser() # Init arguments
     validate_arguments(label=args.label, atoms=args.atoms, us=args.uniform_selector, offset=args.offset, count=args.count) # Validate arguments
-    
+    log.info("Beginning splittor")
     #Reads configuration and generate sample keys. 'key' keeps track of usage of different sampler if used in chain. See the sampler_config() for more comments
     configs = read(RawDataset.extract_path(label=args.label), ':')
     key = sample_config(db=configs, count=args.count, us=args.uniform_selector, offset=args.offset)
@@ -126,7 +139,8 @@ def main():
     #Extracts the sampled config and calculats forces and energues for extracted configs only + for isolated atoms
     calculated_configs = calculate(db=configs, key=key, calculator=args.calculator, atoms=args.atoms)
 
-    save_config(args.label, db=calculated_configs, calc=args.calculator, key=key)
+    save_config(args.out_label, db=calculated_configs, calc=args.calculator, key=key)
+    log.info("Splitter job's done!!!")
 
 
 if __name__=="__main__":
