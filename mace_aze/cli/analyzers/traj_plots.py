d4c5411c31d9aa7db486a69ae13c7ddb974f82e9
Rubin Singh <singhrubin08@gmail.com>
2025-06-21 14:21:19 +0530

    bug fixes

--- mace_aze/cli/analyzers/traj_plots.py
+++ mace_aze/cli/analyzers/traj_plots.py
@@ -5,14 +5,13 @@ def arg_parse():
     parser = argparse.ArgumentParser()
     # parser.add_argument('--models-path', type=str, nargs='+', required=True, help="Space separated model paths")
     parser.add_argument('--traj-path', type=str, required=True, help="Path to trjectory file")
-    parser.add_argument('--timestep', type=int, default=1, help="Time steps of of simualation")
-    parser.add_argument('--interval', type=int, default=10, help="Time interval it was saved")
+    parser.add_argument('--interval', type=int, default=10, help="Time interval between framesm were saved")
     parser.add_argument('--out', type=str, required=True, help="Output directory")
     return parser.parse_args()
 
 def main():
     args = arg_parse()
-    plot_models(traj_path=args.traj_path, time_unit=args.timestep, save_interval=args.interval, output_dir=args.out)
+    plot_models(traj_path=args.traj_path,  save_interval=args.interval, output_dir=args.out)
 
 
 if __name__=="__main__":
