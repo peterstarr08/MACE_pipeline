671fb762a6c55e0d540457e1640f40bb8e9242c7
Rubin Singh <singhrubin08@gmail.com>
2025-06-21 15:32:34 +0530

    isolated atom temp fix

--- mace_aze/cli/iter_trainset.py
+++ mace_aze/cli/iter_trainset.py
@@ -16,7 +16,7 @@ def arg_parse():
     parser.add_argument('--method', type=str, choices=['nikhilboi', 'top'], required=True, help="Method of sampling")
     parser.add_argument('--threshold', type=float, default=-1.0, help="Threshold for sampling. Required by 'nikhil")
     parser.add_argument('--count', type=int, default=-1, help="Number of samples to choose. Required  by 'top'")
-    parser.add_argument('--prev-dataset', type=str, default=None, help="Add path to dataset you want to append to.")
+    parser.add_argument('--prev-dataset', type=str, default=None, required=True, help="Add path to dataset you want to append to.")
     parser.add_argument('--calculator', type=str, choices=['xtb'], default='xtb', help="Calcaultor to evaluate the selected configurations")
     parser.add_argument('--out', type=str, required=True, help="Path to saving the .xyz file")
     return parser.parse_args()
@@ -35,6 +35,7 @@ def main():
     args = arg_parse()
     validate(meth=args.method, count=args.count, threshold=args.threshold)
     log.debug("Arguments validated")
+    log.warning("=========Isolated Atoms additions are not implemented. Use previous dataset to include them")
 
     traj = read(args.traj, ':')
     dataset = []
@@ -50,13 +51,14 @@ def main():
         traj = top_disagreement_sample(configs=traj, count=args.count, key=mace_max_force_std)
     else:
         log.warning("No sampling method provided. This is unexpected")
-    
+
     log.info("Calcaultor: %s", args.calculator)
     if args.calculator=='xtb':
         XTBCalculator().calculate(traj)
 
     dataset = dataset + traj
     log.info("Total dataset size: %d", len(dataset))
+
     out_path = Path(args.out)
     out_path.parent.mkdir(parents=True, exist_ok=True)
     log.info("Writing to %s", str(out_path))
