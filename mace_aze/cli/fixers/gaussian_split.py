d367d8095b5e71b5cbbe6bedf8a6c8a268acc1e5
Rubin Singh <singhrubin08@gmail.com>
2025-06-28 13:34:27 +0530

    added gaussian split support

--- mace_aze/cli/fixers/gaussian_split.py
+++ mace_aze/cli/fixers/gaussian_split.py
@@ -7,7 +7,8 @@ method_basis = ['6-31G(d)']
 
 def arg_parse():
     parser = argparse.ArgumentParser(description="Splits a .xyz trajectory to many gaussian input files for calculating energy and forces on each atom")
-    parser.add_argument('--traj', type=str, required=True, help="Path to trajectory")
+    parser.add_argument('--file', type=str, required=True, help="Path to trajectory")
+    parser.add_argument('--split', type=int, default=1, help="If you want to split the files into multiple folders")
     parser.add_argument('--method', type=str, choices=method_choice, default=method_choice[0], help="DFT method for Gaussian")
     parser.add_argument('--basis', type=str, choices=method_basis, default=method_basis[0], help="DFT basis set")
     parser.add_argument('--dir', type=str, required=True, help="Output directory.\n Warning: It overwrites any exiisitng files")
@@ -16,9 +17,10 @@ def arg_parse():
 def main():
     args = arg_parse()
     xyz_to_com(
-        xyz_file=args.traj,
+        xyz_file=args.file,
         output_dir=args.dir,
         method=args.method,
+        split=args.split,
         basis=args.basis
     )
 
--- mace_aze/pipe/calculators/gaussian/xyz_to_com.py
+++ mace_aze/pipe/calculators/gaussian/xyz_to_com.py
@@ -6,7 +6,7 @@ from mace_aze.log.conf import get_logger
 
 log = get_logger(__name__)
 
-def xyz_to_com(xyz_file: str, output_dir: str, method: str, basis: str, charge=0, mult=1):
+def xyz_to_com(xyz_file: str, output_dir: str, method: str, basis: str, split: int = 1, charge=0, mult=1):
     log.info("Converting xyz to gaussian input files")
     log.debug("Method: %s   Basis: %s", method, basis)
     log.debug("Charge: %s   Multiplicity: %s", str(charge), str(mult))
@@ -16,20 +16,35 @@ def xyz_to_com(xyz_file: str, output_dir: str, method: str, basis: str, charge=0
 
     log.info("Reading %s", xyz_file)
     frames = read(xyz_file, index=':', format='extxyz')
-    log.debug("Read %d frames", len(frames))
+    n_frames = len(frames)
+    log.debug("Read %d frames", n_frames)
 
-    for i, atoms in enumerate(frames):
-        log.debug("Generating frame_%d.com", i)
-        com_path = os.path.join(output_dir, f'frame_{i}.com')
-        with open(com_path, 'w') as f:
-            f.write(f"%chk=frame_{i}.chk\n")
-            f.write(f"# {method}/{basis} Force\n\n")
-            f.write(f"Frame {i} SPE + Forces\n\n")
-            f.write(f"{charge} {mult}\n")
+    # compute chunk size
+    chunk_size = (n_frames + split - 1) // split  # ceiling division
 
-            for atom in atoms:
-                sym = atom.symbol
-                x, y, z = atom.position
-                f.write(f"{sym}  {x:.6f}  {y:.6f}  {z:.6f}\n")
-            f.write("\n") # .com files requires a blank just after atoms position
+    for i in range(split):
+        start = i * chunk_size
+        end = min((i + 1) * chunk_size, n_frames)
+        subfolder = os.path.join(output_dir, f"com_{start}_{end - 1}")
+
+        log.info("Creating folder %s", str(subfolder))
+        os.makedirs(subfolder, exist_ok=True)
+
+        for j in range(start, end):
+            atoms = frames[j]
+            com_path = os.path.join(subfolder, f'frame_{j}.com')
+            log.debug("Generating %s", com_path)
+            with open(com_path, 'w') as f:
+                f.write(f"%chk=frame_{j}.chk\n")
+                f.write(f"# {method}/{basis} Force\n\n")
+                f.write(f"Frame {j} SPE + Forces\n\n")
+                f.write(f"{charge} {mult}\n")
+
+                for atom in atoms:
+                    sym = atom.symbol
+                    x, y, z = atom.position
+                    f.write(f"{sym}  {x:.6f}  {y:.6f}  {z:.6f}\n")
+                f.write("\n")
+        log.info("Written from %d to %d", start, end)
+    
     log.info("Done !!!")
